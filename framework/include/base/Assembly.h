28eefca2c4d8a1a14f25b6074c0dbc7dc7a259ff
WilkAndy <andrew.wilkins@csiro.au>
2018-11-05 20:43:29 +0800

    all Jacobian contributions now included

--- framework/include/base/Assembly.h
+++ framework/include/base/Assembly.h
@@ -951,6 +951,11 @@ public:
    * Set the pointer to the XFEM controller object
    */
   void setXFEM(std::shared_ptr<XFEMInterface> xfem) { _xfem = xfem; }
+  void addJacobianBlock(SparseMatrix<Number> & jacobian,
+                        DenseMatrix<Number> & jac_block,
+                        const std::vector<dof_id_type> & idof_indices,
+                        const std::vector<dof_id_type> & jdof_indices,
+                        Real scaling_factor);
 
 protected:
   /**
@@ -993,12 +998,6 @@ protected:
   void setResidualBlock(NumericVector<Number> & residual,
                         DenseVector<Number> & res_block,
                         std::vector<dof_id_type> & dof_indices,
-                        Real scaling_factor);
-
-  void addJacobianBlock(SparseMatrix<Number> & jacobian,
-                        DenseMatrix<Number> & jac_block,
-                        const std::vector<dof_id_type> & idof_indices,
-                        const std::vector<dof_id_type> & jdof_indices,
                         Real scaling_factor);
 
   /**
