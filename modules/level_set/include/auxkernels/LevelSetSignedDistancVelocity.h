d84cdf339d094262773342579615316df00eab61
shoheiogawa <shoheio@andrew.cmu.edu>
2018-11-29 19:06:07 -0500

    Changed implementation.

--- modules/level_set/include/auxkernels/LevelSetSignedDistancVelocity.h
+++ modules/level_set/include/auxkernels/LevelSetSignedDistancVelocity.h
@@ -7,8 +7,8 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#ifndef LEVEL_SET_SIGNED_DISTANC_VELOCITY_H
-#define LEVEL_SET_SIGNED_DISTANC_VELOCITY_H
+#ifndef LEVEL_SET_SIGNED_DISTANCE_VELOCITY_H
+#define LEVEL_SET_SIGNED_DISTANCE_VELOCITY_H
 
 #include "AuxKernel.h"
 
@@ -26,15 +26,22 @@ public:
 protected:
   virtual Real computeValue() override;
 
+  Real smoothStepFunction(Real psi) const;
+
   // Distance value variable and its gradient
   const VariableValue & _psi;
   const VariableGradient & _grad_psi;
 
+  const VariableValue & _psi_0;
+
   // For the selection of x, y, or z
   const MooseEnum & _component;
+
+  // Value for smoothed distance function
+  const Real _epsilon;
 
   // Scale for the interface velocity
   const Real _scale;
 };
 
-#endif // LEVEL_SET_SIGNED_DISTANC_VELOCITY_H
+#endif // LEVEL_SET_SIGNED_DISTANCE_VELOCITY_H
--- modules/level_set/src/auxkernels/LevelSetSignedDistancVelocity.C
+++ modules/level_set/src/auxkernels/LevelSetSignedDistancVelocity.C
@@ -9,6 +9,8 @@
 
 #include "LevelSetSignedDistancVelocity.h"
 
+#include <cmath>
+
 registerMooseObject("LevelSetApp", LevelSetSignedDistancVelocity);
 
 template <>
@@ -19,6 +21,8 @@ validParams<LevelSetSignedDistancVelocity>()
   MooseEnum comp("x=0 y=1 z=2");
   params.addRequiredParam<MooseEnum>("component", comp, "The component of velocity to return.");
   params.addRequiredCoupledVar("psi", "The variable for signed distance value.");
+  params.addRequiredCoupledVar("psi_0", "The initial value of the variable for signed distance value.");
+  params.addRequiredParam<Real>("epsilon",  "epsilon value for smoothed distance function.");
   params.addParam<Real>("scale", 1.0, "The scale value for the velocity vector.");
   return params;
 }
@@ -27,7 +31,9 @@ LevelSetSignedDistancVelocity::LevelSetSignedDistancVelocity(const InputParamete
   : AuxKernel(parameters),
     _psi(coupledValue("psi")),
     _grad_psi(coupledGradient("psi")),
+    _psi_0(coupledValue("psi_0")),
     _component(getParam<MooseEnum>("component")),
+    _epsilon(getParam<Real>("epsilon")),
     _scale(getParam<Real>("scale"))
 {
 }
@@ -36,15 +42,25 @@ Real
 LevelSetSignedDistancVelocity::computeValue()
 {
   Real psi = _psi[_qp];
-  Real sign = (psi > 0) - (psi < 0);
+  Real psi_0 = _psi_0[_qp];
   Real grad_psi_norm = _grad_psi[_qp].norm();
+  Real S_0_psi_0 = 2.0 * smoothStepFunction(psi_0) - 1.0;
+
   if (grad_psi_norm < 1e-6)
     return 0.0;
   else
   {
-    if (psi < -0.5 || 0.5 < psi)
-      return 0.0;
-    else
-      return _scale * sign * _grad_psi[_qp](_component) / grad_psi_norm;
+    return _scale * S_0_psi_0 * _grad_psi[_qp](_component) / grad_psi_norm;
   }
+}
+
+Real
+LevelSetSignedDistancVelocity::smoothStepFunction(Real psi) const
+{
+  if (psi < -_epsilon)
+    return 0.0;
+  else if (-_epsilon <= psi && psi <= _epsilon)
+    return 1.0 / 2.0 * (1.0 + psi / _epsilon + 1.0 / M_PI * sin(M_PI * psi / _epsilon));
+  else
+    return 1.0;
 }
