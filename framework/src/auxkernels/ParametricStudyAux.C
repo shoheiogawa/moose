231f515ec8218a7bd656d698fc3309772ad537d8
shoheiogawa <shoheio@andrew.cmu.edu>
2019-01-15 01:03:34 -0500

    Refactored. Added a test without a mesh refinement.

--- framework/src/auxkernels/ParametricStudyAux.C
+++ framework/src/auxkernels/ParametricStudyAux.C
@@ -21,23 +21,26 @@ InputParameters
 validParams<ParametricStudyAux>()
 {
   InputParameters params = validParams<AuxKernel>();
-  params.addClassDescription("Aux kernel that sets values to the aux variable. "
-                             "The parameters are given either as a list or a range function, "
-                             "which is similar to `range` function in Python. If the time: t is between `tolerance` to 1 + `tolerance`, "
-                             "this aux kernel sets the first value. This is repeated until all the parameters are studied. ");
+  params.addClassDescription("Aux kernel that sets values to an aux variable. "
+                             "The parameters are given as a list or range function.");
 
   // Either value_list or range_func should be provided but not both.
-  params.addParam<std::vector<Real>>("value_list", "List of values which you want to set to the aux variable.");
-  params.addParam<std::vector<Real>>("range_func", "Range function which determines "
-                                     "the values which you want to set to the aux variable."
-                                     "Three Real values should be given: start end increment");
+  params.addParam<std::vector<Real>>("value_list", "List of values to be studied.");
+  params.addParam<std::vector<Real>>("range_func", "Range function which provides values to study. "
+                                                   "Three Real values (start end increment) should be given."
+                                                   "Ex) With arguments: 0.0 0.3 0.1, 0.0, 0.1, 0.2 will be the parameters.");
 
   MooseEnum order("ascending descending");
   params.addParam<MooseEnum>("sort", order, "Sorting order for the params. If it is not specified, "
-                                            "the parameters are the list + the range function results.");
+                                            "the parameters are the list (in the order as written) followed by the range function results.");
 
-  params.addParam<bool>("unique", true, "Set true to make all the parameters unique.");
-  params.addParam<Real>("unique_tolerance", 1e-12, "Tolerance to find two values in the list the same.");
+  params.addParam<bool>("unique_list", true, "Set false to study same parameters twice or more in one simulation.");
+  params.addParam<Real>("unique_tolerance", 1e-12, "Tolerance to find two values in the parameter list the same.");
+
+  params.addParam<Real>("epsilon_range", 1e-12, "Small value used in `(start - end - epsilon_range) / increment` "
+                                                "to compute the number of parameters studied.");
+  params.addParam<Real>("epsilon_index", 1e-12, "Small value used in time to index conversion such that, for example, "
+                                                "at t = 1, the first parameter is studied in stead of the second one.");
 
   // Parameters have to be set at the beginning of each time step because it uses the time value for indexing.
   params.set<ExecFlagEnum>("execute_on") = "TIMESTEP_BEGIN";
@@ -50,15 +53,14 @@ ParametricStudyAux::ParametricStudyAux(const InputParameters & parameters)
   _value_list(getParam<std::vector<Real>>("value_list")),
   _range_func_args(getParam<std::vector<Real>>("range_func")),
   _sort(getParam<MooseEnum>("sort")),
-  _unique_list(getParam<bool>("unique")),
-  _unique_tolerance(getParam<Real>("unique_tolerance"))
+  _unique_list(getParam<bool>("unique_list")),
+  _unique_tolerance(getParam<Real>("unique_tolerance")),
+  _epsilon_range(getParam<Real>("epsilon_range")),
+  _epsilon_index(getParam<Real>("epsilon_index"))
 {
   // The simulation has to be transient for indexing.
   if (!(_subproblem.isTransient()))
     mooseError("ParametricStudyAux works only in a transient simulation.");
-
-  //if (isParamValid("range_func") && isParamValid("value_list"))
-  //  mooseWarning("value_list and value_range are given. Only one is accepted.");
 
   // If we use value_range, check if there are exactly three values given to the parameter.
   // Add the range function results to _value_list, which may have some values already.
@@ -71,7 +73,7 @@ ParametricStudyAux::ParametricStudyAux(const InputParameters & parameters)
     Real end_value   = _range_func_args[1];
     Real increment   = _range_func_args[2];
 
-    Real num_of_params_to_generate = (end_value - start_value) / increment;
+    Real num_of_params_to_generate = (end_value - start_value - _epsilon_range) / increment;
     if (num_of_params_to_generate < 1.0)
       mooseError("No sweep parameter is generated using `range_func` parameter.");
 
@@ -91,7 +93,7 @@ ParametricStudyAux::ParametricStudyAux(const InputParameters & parameters)
 
   if (_unique_list)
   {
-    // Remove consecutive duplicates in range because if we study for the same parameter,
+    // Remove consecutive duplicates in range. If we study for the same parameter,
     // the simulation fails because the solution doesn't change at all for the second instance of the same parameter.
     std::vector<Real>::iterator it;
     it = std::unique(_value_list.begin(), _value_list.end(), [this](Real a, Real b) {return std::fabs(a - b) < _unique_tolerance;});
@@ -115,15 +117,15 @@ ParametricStudyAux::ParametricStudyAux(const InputParameters & parameters)
 void
 ParametricStudyAux::timestepSetup()
 {
-  unsigned int index = static_cast<unsigned int>(_t - 1e-15);
+  unsigned int index = static_cast<unsigned int>(_t - _epsilon_index);
   _console << "Parametric study : " + _var.name() << " = " << _value_list[index] << " at time = " << _t << "\n";
-  if (_t + 1e-15 < ceil(_t - 1e-15))
+  if (_t + _epsilon_index < ceil(_t - _epsilon_index))
     _console << "(This time step is intermediate for mesh refinement.)\n";
 }
 
 Real
 ParametricStudyAux::computeValue()
 {
-  unsigned int index = static_cast<unsigned int>(_t - 1e-15);
+  unsigned int index = static_cast<unsigned int>(_t - _epsilon_index);
   return _value_list[index];
 }
